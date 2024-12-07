b8b808bbe98e33856ff97e7b61547c2686a8a12b
satya1234224 <satya27033@gmail.com>
2024-12-08 04:17:04 +0530

    Update Doctorlogin.jsx

--- frontend/src/Components/User/Login/Doctorlogin.jsx
+++ frontend/src/Components/User/Login/Doctorlogin.jsx
@@ -22,7 +22,7 @@ function LoginForm() {
   const onSubmit = async (values) => {
     // console.log(values);
     try {
-        const response= await axios.post ("http://localhost:8080/doctorsignin",values)
+        const response= await axios.post ("https://hospital-management-stam.onrender.com/doctorsignin",values)
         console.log(response.data)
         
         if(response.status===200){
