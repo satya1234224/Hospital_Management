c9c8b20e7f23791106e0241d08e2d9a68d5b65e4
satya1234224 <satya27033@gmail.com>
2024-12-08 04:15:11 +0530

    Update Users.jsx

--- frontend/src/Components/Doctor/components/Users.jsx
+++ frontend/src/Components/Doctor/components/Users.jsx
@@ -16,7 +16,7 @@ export default function Users() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:8080/appointments', {
+      const response = await axios.get('https://hospital-management-stam.onrender.com/appointments', {
         headers: {
           'Content-Type': 'application/json',
           authorization: localStorage.getItem('jwt'),
